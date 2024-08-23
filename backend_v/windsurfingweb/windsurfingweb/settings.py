367f669cb6029775cbe5ac57561bf8b2aab1ab2d
Takumi <sakai.takumi.54m@st.kyoto-u.ac.jp>
2024-08-23 11:13:23 +0900

    AWS

--- backend_v/windsurfingweb/windsurfingweb/settings.py
+++ backend_v/windsurfingweb/windsurfingweb/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vhdk_4s++segj=^pnvy$9lzb(4q31=km1e#jm6!8s+=t(yf9^q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
